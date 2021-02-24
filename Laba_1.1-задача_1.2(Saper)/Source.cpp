13da7f4610dcc642e5104aa6cb8b070857583130
Kirill Sharapov <62686901+Robingood43@users.noreply.github.com>
2021-02-24 22:55:30 +0600

    Add files via upload

---
+++ Laba_1.1-задача_1.2(Saper)/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+int main()
+{
+	int n, m;
+	int counter = 0;
+	while (true)
+	{
+		std::cin >> m >> n;// вводим границы
+		switch (m || n)
+			case 0: break;
+		counter++;//какой по счету случай
+
+		int field[5][5] = { 0 };
+		for (int i = 1; i < m + 1; i++)
+			for (int j = 1; j < n + 1; j++)
+			{
+				char temp;
+				std::cin >> temp;
+				if (temp == '*')
+				{
+					//111
+					//1*1
+					//111
+					for (int ii = i - 1; ii <= i + 1; ++ii)
+						for (int jj = j - 1; jj <= j + 1; ++jj)
+							if (field[ii][jj] != -1)
+								++field[ii][jj];
+
+					field[i][j] = -1;
+				}
+
+			}
+
+		std::cout << "Field #" << counter << ":\n";
+		for (int i = 1; i < m + 1; i++)
+		{
+
+			for (int j = 1; j < n + 1; j++)
+			{
+				switch (field[i][j])
+				{
+				case -1: std::cout << '*'; break;// если мина (*) рисуем мину
+				default:
+					std::cout << field[i][j];// иначе рисуем цифру равну кол-ву мин рядом
+					break;
+				}
+			}
+			std::cout << "\n";
+
+		}
+		std::cout << "\n";
+	}
+
+	return 0;
+}
